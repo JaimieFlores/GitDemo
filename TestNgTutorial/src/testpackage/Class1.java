610bce1a7f4cb8837dd9faaef881de3d0e4178d9
Jaimie <jaimiecharlene.flores@stratpoint.com>
2023-01-20 10:32:26 +0800

    first commit

---
+++ TestNgTutorial/src/testpackage/Class1.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+	@Test
+	public static void Demo () {
+		// TODO Auto-generated method stub
+		System.out.println("Hey");
+	}
+
+}
---
+++ TestNgTutorial/src/testpackage/Class2.java
@@ -0,0 +1,16 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+	
+	@Test
+	public static void Demo2 () {
+		// TODO Auto-generated method stub
+		System.out.println("Great");
+
+	}
+}
+
+
